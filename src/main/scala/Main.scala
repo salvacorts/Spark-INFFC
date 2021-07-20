69b51eeb4bf31c2e00c0a1446884a1379ac5ca17
Salva Corts <salva@rti.com>
2021-07-20 21:14:52 +0200

    Get Neighbours and classes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,6 +7,8 @@ import org.apache.spark.ml.knn.KNN
 import scala.collection.mutable
 
 object Main {
+    val K = 5
+
     def main(args: Array[String]) {
         val spark = SparkSession.builder.appName("HelloWorld Application").getOrCreate()
 
@@ -33,24 +35,31 @@ object Main {
         df_ml = indexer.fit(df_ml).transform(df_ml)
         df_ml.show()
 
-        val knn = new KNN().
-            setTopTreeSize(10).
-            setAuxCols(Array("features"))
+        val knn = new KNN()
+            .setTopTreeSize(10)
+            .setFeaturesCol("features")
+            .setAuxCols(Array("features", "varietyInt"))
 
-        val model = knn.fit(df_ml).setK(5)
+        // K+1 Since KNN returns the sample itself as a neighbour,
+        // we will discard it. https://github.com/saurfang/spark-knn/issues/7
+        val model = knn.fit(df_ml).setK(K+1)
 
         val results_df = model.transform(df_ml)
         results_df.printSchema()
         results_df.show()
 
-        results_df.collect.foreach(row => {
-            val original_features = row.getAs[Vector]("features")
-            println("Features: " + original_features)
+        results_df.collect.foreach(sample => {
+            val original_features = sample.getAs[Vector]("features")
+            val original_variety = sample.getAs[Double]("varietyInt")
+            println(s"\nSample: $original_features - $original_variety")
 
-            val neighbors_features = row.getAs[mutable.WrappedArray[Row]]("neighbors")
-                .map(_.getAs[Vector]("features"))
-            neighbors_features.foreach(features => {
-                println("Neighbor features: " + features)
+            val neighbors = sample.getAs[mutable.WrappedArray[Row]]("neighbors")
+                .takeRight(K) // Skips the first one which is this same point
+
+            neighbors.foreach(neighbor => {
+                val features = neighbor.getAs[Vector]("features")
+                val variety = neighbor.getAs[Double]("varietyInt")
+                println(s"Neighbor: $features - $variety")
             })
         })
 
